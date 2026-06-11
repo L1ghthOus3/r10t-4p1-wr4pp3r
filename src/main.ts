437f13964745a5fc747a1c4b475c29652c6e982b
Tristan GORY <72013641+Sunatsirt@users.noreply.github.com>
2026-06-11 09:52:01 +0200

    refactor: changed cors handling

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: process.env.CORS_ORIGIN?.split(',') ?? true });
+  app.enableCors({
+    origin: [
+      'https://5umm0n3r5-5c4nn3r.dev',
+      'http://localhost:5500',
+      'http://localhost:3000',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: false,
+  });
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
   await app.listen(process.env.PORT ?? 3000);
 }
